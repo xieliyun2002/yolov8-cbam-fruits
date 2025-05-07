cc4b7a4db628bc2fa97f089117cbea34434087ae
xieliyun2002 <62679239+xieliyun2002@users.noreply.github.com>
2025-05-07 20:17:27 +0800

    tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -432,21 +432,21 @@ class DetectionModel(BaseModel):
         i = (y[-1].shape[-1] // g) * sum(4 ** (nl - 1 - x) for x in range(e))  # indices
         y[-1] = y[-1][..., i:]  # small
         return y
-def init_criterion(self):
-    from ultralytics.utils.loss import E2EDetectLoss, v8DetectionLoss
-    from ultralytics.losses import ClassBalancedFocalLoss, CBFLossWrapper
+    def init_criterion(self):
+        from ultralytics.utils.loss import E2EDetectLoss, v8DetectionLoss
+        from ultralytics.losses import ClassBalancedFocalLoss, CBFLossWrapper
 
-    base_loss = E2EDetectLoss(self) if getattr(self, "end2end", False) else v8DetectionLoss(self)
+        base_loss = E2EDetectLoss(self) if getattr(self, "end2end", False) else v8DetectionLoss(self)
 
-    samples_per_cls = [44, 551, 71, 200, 997, 137, 192, 661, 335, 136,
+        samples_per_cls = [44, 551, 71, 200, 997, 137, 192, 661, 335, 136,
                        1119, 767, 203, 331, 105, 325, 302, 137, 239,
                        830, 439, 358, 176, 1364, 151, 397, 47, 101,
                        531, 327, 181, 349, 281, 265, 64, 344]
-    cbfl = ClassBalancedFocalLoss(samples_per_cls, beta=0.9999, gamma=2.0)
+        cbfl = ClassBalancedFocalLoss(samples_per_cls, beta=0.9999, gamma=2.0)
 
-    cls_w = getattr(self, 'args', {}).get('cls', getattr(self, 'hyp', {}).get('cls', 1.0))
+        cls_w = getattr(self, 'args', {}).get('cls', getattr(self, 'hyp', {}).get('cls', 1.0))
 
-    return CBFLossWrapper(base_loss, cbfl, cls_w)
+        return CBFLossWrapper(base_loss, cbfl, cls_w)
 
 
 
