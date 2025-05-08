01b0b53defba6a2a23a5c8813126328edd87ac19
xieliyun2002 <62679239+xieliyun2002@users.noreply.github.com>
2025-05-08 13:14:39 +0800

    tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -431,8 +431,19 @@ class DetectionModel(BaseModel):
         return y
         
     def init_criterion(self):
+        from my_losses import ClassBalancedFocalLoss, CBFLossWrapper   # ← 根据实际路径修改
         from ultralytics.utils.loss import v8DetectionLoss
-        return v8DetectionLoss(self) 
+
+        base_loss = v8DetectionLoss(self)
+        samples_per_cls = [44, 551, 71, 200, 997, 137, 192, 661, 335, 136,
+                       1119, 767, 203, 331, 105, 325, 302, 137, 239,
+                       830, 439, 358, 176, 1364, 151, 397, 47, 101,
+                       531, 327, 181, 349, 281, 265, 64, 344]
+        cbfl  = ClassBalancedFocalLoss(samples_per_cls, beta=0.9999, gamma=2.0)
+        cls_w = self.args.get("cls", 1.0)    # 读取 --cls 参数
+
+        return CBFLossWrapper(base_loss, cbfl, cls_w)
+
 
 
 
