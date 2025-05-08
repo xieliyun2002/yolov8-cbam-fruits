d5a5bf89b96f69fa5b1fdff2ce9cc1e1ebb189d4
xieliyun2002 <62679239+xieliyun2002@users.noreply.github.com>
2025-05-08 12:45:53 +0800

    loss.py

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -262,13 +262,13 @@ class v8DetectionLoss:
         # 这里默认 -1 表示负样本，跳过
         for b in range(B):
             for n in range(N):
-            cls_id_tensor = target_scores[b, n]
-            if cls_id_tensor.numel() == 1:
-                cls_id = int(cls_id_tensor.item())
-            else:
-                cls_id = int(cls_id_tensor.argmax().item())  # ✅ 获取最大值对应的类别索引
-            if 0 <= cls_id < C:
-                target_scores_[b, n, cls_id] = 1.0
+                cls_id_tensor = target_scores[b, n]
+                if cls_id_tensor.numel() == 1:
+                    cls_id = int(cls_id_tensor.item())
+                else:
+                    cls_id = int(cls_id_tensor.argmax().item())  # ✅ 获取最大值对应的类别索引
+                if 0 <= cls_id < C:
+                    target_scores_[b, n, cls_id] = 1.0
 
 
         target_scores = target_scores_.to(dtype).to(pred_scores.device)
