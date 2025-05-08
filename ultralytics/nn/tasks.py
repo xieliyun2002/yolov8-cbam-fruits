cc89f55ea4687c558182c59ee4f770c1cc1fa025
xieliyun2002 <62679239+xieliyun2002@users.noreply.github.com>
2025-05-08 13:54:18 +0800

    tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -439,10 +439,16 @@ class DetectionModel(BaseModel):
                        830, 439, 358, 176, 1364, 151, 397, 47, 101,
                        531, 327, 181, 349, 281, 265, 64, 344]
         cbfl = ClassBalancedFocalLoss(samples_per_cls)
-        return CBFLossWrapper(base_loss, cbfl,
-                          cls_w,
-                          self.model[-1].nc,
-                          self.model[-1].reg_max)
+        cls_w = getattr(self, "args", {}).get("cls", 1.0)
+
+        return CBFLossWrapper(
+            base_loss,
+            cbfl,
+            cls_w,
+            self.model[-1].nc,
+            self.model[-1].reg_max,
+        )
+
 
 
 
